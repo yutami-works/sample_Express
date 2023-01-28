61d4f2317e7c7578c991069050f836cb95f393b4
yutami <sabacan623@gmail.com>
2023-01-28 19:29:55 +0900

    ページ追加の基本

---
+++ routes/page.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+// ここには相対的なルーティングを書く（下は/page/にアクセスした時の処理）
+router.get('/', (req, res, next) => {
+  var data = {
+    title:'Hello!',
+    content:'これは、サンプルのコンテンツです。<br>this is sample content.'
+  };
+  res.render('page', data);
+});
+
+module.exports = router;
